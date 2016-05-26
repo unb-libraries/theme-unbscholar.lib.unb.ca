48d4665e8353d1ee29860929ffa6dd42af275fb4
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2016-05-26 14:17:57 -0300

    Close BugzID: 4025 (alter username description text on login form)

--- template.php
+++ template.php
@@ -2,5 +2,15 @@
 
 /**
  * @file
- * template.php
+ * Hooks for the unbscholar_lib_unb_ca subtheme.
  */
+
+/**
+ * Implements hook_form_alter.
+ */
+function unbscholar_lib_unb_ca_form_alter(&$form, &$form_state, $form_id) {
+  // Reword default description text for Drupal login form username input widget.
+  if($form_id == 'user_login') {
+    $form['name']['#description'] = t('Enter your UNB Login ID.');
+  }
+}
